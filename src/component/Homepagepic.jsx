8644c997bc5a1b8736b2e80c075e625057b63c5d
yapcheng <yangterry0@gmail.com>
2025-04-18 09:18:38 +0800

    Update Homepagepic.jsx

--- src/component/Homepagepic.jsx
+++ src/component/Homepagepic.jsx
@@ -1,7 +1,7 @@
 function HomepagePic() {
   return (
     <div className="flex justify-center bg-black h-[50vh] "> 
-    <img className=' w-auto h-full overflow-hidden' src="/images/ramen.jpg" alt="Ramen" />
+    <img className=' w-full h-full overflow-hidden' src="/images/ramen.jpg" alt="Ramen" />
   </div>
   );
 }
