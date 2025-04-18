8171d618d53bbade1666ffac1dddb911b3e846d8
yapcheng <yangterry0@gmail.com>
2025-04-18 09:02:28 +0800

    Update Homepagepic.jsx

--- src/component/Homepagepic.jsx
+++ src/component/Homepagepic.jsx
@@ -1,7 +1,7 @@
 function HomepagePic() {
   return (
     <div className="flex justify-center bg-black h-[50vh] "> 
-    <img className=' w-atuo h-full overflow-hidden' src="./public/images/ramen.jpg" alt="Ramen" />
+    <img className=' w-atuo h-full overflow-hidden' src="../public/images/ramen.jpg" alt="Ramen" />
   </div>
   );
 }
